7f0e60d22e8a12e98c1730c5034a3b8e683785ba
Mike <mickiller.25@gmail.com>
2019-06-01 12:26:05 -0500

    Cleanup and Add some args
    Add file argument
    Cleanup
    Format python file

--- harris_corner_detector_clase04/harris.py
+++ harris_corner_detector_clase04/harris.py
@@ -1,76 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
 import numpy
-import pylab
 import matplotlib.pyplot as plt
 import cv2
 from scipy.ndimage import filters
-from PIL import Image
+from os import path as p
+# import pylab
+# from PIL import Image
+
+_FILEPATH = p.abspath('../imagenes_proyectos/')
+__version__ = "0.0.1"
 
 
-def compute_harris_response(im,sigma=10):
-	""" Compute the Harris corner detector response function
-	for each pixel in a graylevel image. """
-	# derivatives
-	imx = numpy.zeros(im.shape)
-	filters.gaussian_filter(im, (sigma,sigma), (0,1), imx)
-	imy = numpy.zeros(im.shape)
-	filters.gaussian_filter(im, (sigma,sigma), (1,0), imy)
-	# compute components of the Harris matrix
-	Wxx = filters.gaussian_filter(imx*imx,sigma)
-	Wxy = filters.gaussian_filter(imx*imy,sigma)
-	Wyy = filters.gaussian_filter(imy*imy,sigma)
-	# determinant and trace
-	Wdet = Wxx*Wyy - Wxy**2
-	Wtr = Wxx + Wyy
-	return Wdet / Wtr
+def compute_harris_response(im, sigma=15):
+    """ Compute the Harris corner detector response function
+    for each pixel in a graylevel image. """
+    # derivatives
+    imx = numpy.zeros(im.shape)
+    filters.gaussian_filter(im, (sigma, sigma), (0, 1), imx)
+    imy = numpy.zeros(im.shape)
+    filters.gaussian_filter(im, (sigma, sigma), (1, 0), imy)
+    # compute components of the Harris matrix
+    Wxx = filters.gaussian_filter(imx*imx, sigma)
+    Wxy = filters.gaussian_filter(imx*imy, sigma)
+    Wyy = filters.gaussian_filter(imy*imy, sigma)
+    # determinant and trace
+    Wdet = Wxx*Wyy - Wxy**2
+    Wtr = Wxx + Wyy
+    return Wdet / Wtr
 
 
-def get_harris_points(harrisim,min_dist=10,threshold=0.1):
-	""" Return corners from a Harris response image min_dist is the minimum number of pixels separating corners and image boundary. """
-	# find top corner candidates above a threshold
-	corner_threshold = harrisim.max() * threshold
-	harrisim_t = (harrisim > corner_threshold) * 1
-	# get coordinates of candidates
-	coords = numpy.array(harrisim_t.nonzero()).T
-	# ...and their values
-	candidate_values = [harrisim[c[0],c[1]] for c in coords]
-	# sort candidates
-	index = numpy.argsort(candidate_values)
-	# store allowed point locations in array
-	allowed_locations = numpy.zeros(harrisim.shape)
-	allowed_locations[min_dist:-min_dist,min_dist:-min_dist] = 1
-	# select the best points taking min_distance into account
-	filtered_coords = []
-	for i in index:
-		if allowed_locations[coords[i,0],coords[i,1]] == 1:
-			filtered_coords.append(coords[i])
-			allowed_locations[(coords[i,0]-min_dist):(coords[i,0]+min_dist),
-				(coords[i,1]-min_dist):(coords[i,1]+min_dist)] = 0
-	return filtered_coords
+def get_harris_points(harrisim, min_dist=10, threshold=0.1):
+    """ Return corners from a Harris response image min_dist is the minimum number of pixels separating corners and image boundary. """
+    # find top corner candidates above a threshold
+    corner_threshold = harrisim.max() * threshold
+    harrisim_t = (harrisim > corner_threshold) * 1
+    # get coordinates of candidates
+    coords = numpy.array(harrisim_t.nonzero()).T
+    # ...and their values
+    candidate_values = [harrisim[c[0], c[1]] for c in coords]
+    # sort candidates
+    index = numpy.argsort(candidate_values)
+    # store allowed point locations in array
+    allowed_locations = numpy.zeros(harrisim.shape)
+    allowed_locations[min_dist:-min_dist, min_dist:-min_dist] = 1
+    # select the best points taking min_distance into account
+    filtered_coords = []
+    for i in index:
+        if allowed_locations[coords[i, 0], coords[i, 1]] == 1:
+            filtered_coords.append(coords[i])
+            allowed_locations[(coords[i, 0]-min_dist):(coords[i, 0]+min_dist), (coords[i, 1]-min_dist):(coords[i, 1]+min_dist)] = 0
+    return filtered_coords
 
 
-def plot_harris_points(img_points,img_blur,img_real,filtered_coords):
-	""" Plots corners found in image. """
-	fig = plt.figure()
-	im1= plt.subplot(221)
-	plt.gray()
-	plt.imshow(img_real)
-	plt.axis('off')
-	im2= plt.subplot(222)
-	plt.imshow(img_blur)
-	plt.axis('off')
-	im3= plt.subplot(223)
-	plt.imshow(img_points)
-	plt.plot([p[1] for p in filtered_coords],[p[0] for p in filtered_coords],'*')
-	plt.axis('off')
-	im1.title.set_text('real')
-	im2.title.set_text('blur')
-	im3.title.set_text('key points')
-	plt.show()
+def plot_harris_points(img_points, img_blur, img_real, filtered_coords):
+    """ Plots corners found in image. """
+    # fig = plt.figure()
+    im1 = plt.subplot(221)
+    plt.gray()
+    plt.imshow(img_real)
+    plt.axis('off')
+    im2 = plt.subplot(222)
+    plt.imshow(img_blur)
+    plt.axis('off')
+    im3 = plt.subplot(223)
+    plt.imshow(img_points)
+    plt.plot([p[1] for p in filtered_coords], [p[0] for p in filtered_coords], '*')
+    plt.axis('off')
+    im1.title.set_text('real')
+    im2.title.set_text('blur')
+    im3.title.set_text('key points')
+    plt.show()
 
-img = cv2.imread('pic03_a.jpg', cv2.IMREAD_GRAYSCALE)
-blur=cv2.GaussianBlur(img,(149,149),0)
+def parseArgs():
+    """TODO: Docstring for parseArgs.
+    :returns: TODO
+
+    """
+    parser = argparse.ArgumentParser(
+        description='Small Harrys dot finding')
+
+    parser.add_argument(
+        '-f',
+        '--file',
+        default='pic03_a',
+        dest='filename',
+        help='Set the file/image to use')
+    # parser.add_argument(
+    #     '-v',
+    #     '--verbose',
+    #     dest='log_level',
+    #     action='store_const',
+    #     const='DEBUG',
+    #     help='a shortcut for --log-level=DEBUG')
+    # parser.add_argument(
+    #     '-q',
+    #     '--quiet',
+    #     dest='log_level',
+    #     action='store_const',
+    #     const='CRITICAL',
+    #     help='a shortcut for --log-level=CRITICAL')
+    # parser.add_argument('--version', action='version', version=__version__)
+
+    return parser.parse_args()
+
+
+args = parseArgs()
+args.filename = p.join(_FILEPATH, args.filename + '.jpg')
+img = cv2.imread(args.filename, cv2.IMREAD_GRAYSCALE)
+blur = cv2.GaussianBlur(img, (149, 149), 0)
 im = numpy.array(blur)
 harrisim = compute_harris_response(im)
-filtered_coords = get_harris_points(harrisim,6)
+filtered_coords = get_harris_points(harrisim, 6)
 plot_harris_points(im, blur, img, filtered_coords)
-
